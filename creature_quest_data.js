2e0e0c31c081461e140f58da4e2bda2dfdf9c390
thecoder.live <82015917+thecoderlive@users.noreply.github.com>
2023-08-18 12:04:49 +0530

    Initial commit for file: creature_quest_data.js

---
+++ creature_quest_data.js
@@ -0,0 +1,1 @@
+export const item = {"background_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/274.png", "id": "ae1118b3-801b-4be4-8fd0-a530527c07e8", "description": "Page throughout company question back line. Benefit story keep"}
